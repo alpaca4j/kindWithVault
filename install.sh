a1521a83f0f7013a7f2f543176e8304348764ba7
Marcin Badura <marcin@badura.io>
2021-02-06 09:39:54 +0100

    rm

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ kubectl apply -f vault-namespace.yaml
 kubectl apply -f vault-auth-sa.yaml
 TOKEN_REVIEW_JWT=$(kubectl get secret vault-auth --output="go-template={{ .data.token }}" | base64 --decode)
 KUBE_CA_CERT=$(kubectl config view --raw --minify --flatten --output="jsonpath={.clusters[].cluster.certificate-authority-data}" | base64 --decode)
-#sed -e "s/\${TOKEN_REVIEW_JWT}/$TOKEN_REVIEW_JWT/" -e "s/\${KUBE_CA_CERT}/$KUBE_CA_CERT/" < configMapInitK8s.template > configMapInitK8s.yaml
 awk -v TOKEN_REVIEW_JWT="$TOKEN_REVIEW_JWT" -v KUBE_CA_CERT="$KUBE_CA_CERT" '{
 sub(/\${TOKEN_REVIEW_JWT}/,TOKEN_REVIEW_JWT);
 sub(/\${KUBE_CA_CERT}/,KUBE_CA_CERT);
